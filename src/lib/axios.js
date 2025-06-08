814e16e39ac1eb271438cc47e5adb298dcdea0cd
MUHAMMAD ANAS <154446804+anashanif123@users.noreply.github.com>
2025-06-08 19:46:42 +0500

    redploy

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://chat-backend-rho-three.vercel.app/api', // Use /api if your backend routes are prefixed with /api
+    baseURL: 'https://chat-backend-sandy-six.vercel.app/api', // Use /api if your backend routes are prefixed with /api
     withCredentials: true,
 });

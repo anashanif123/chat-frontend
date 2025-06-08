94166dd33cd26280402b86beff0fdc2eb70fd686
MUHAMMAD ANAS <154446804+anashanif123@users.noreply.github.com>
2025-06-08 05:46:48 +0500

    add production

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: "https://chat-backend-q5muc2jd8-anas-projects-dfbef841.vercel.app",
-  withCredentials: true,
+    baseURL: 'https://chat-backend-o9kj5b3uo-anas-projects-dfbef841.vercel.app/api', // Use /api if your backend routes are prefixed with /api
+    withCredentials: true,
 });

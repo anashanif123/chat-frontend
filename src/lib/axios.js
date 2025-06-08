a57bfd09a543b705a1e58b18e3437bfc8cc58558
MUHAMMAD ANAS <154446804+anashanif123@users.noreply.github.com>
2025-06-08 05:42:51 +0500

    add production

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://chat-backend-q5muc2jd8-anas-projects-dfbef841.vercel.app", // <-- updated backend URL
+  baseURL: "https://chat-backend-q5muc2jd8-anas-projects-dfbef841.vercel.app",
   withCredentials: true,
 });

bf27aa6391a6a48f04b0c819d0dbe64a35b1dd65
MUHAMMAD ANAS <154446804+anashanif123@users.noreply.github.com>
2025-06-08 20:14:29 +0500

    lets deploy

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://chat-backend-pi-six.vercel.app', // Remove /api if not used in backend
+    baseURL: 'https://chat-backend-pi-six.vercel.app/api', // Remove /api if not used in backend
     withCredentials: true,
 });
